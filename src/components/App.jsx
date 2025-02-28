06516448e6e29d7ad90fa885d8efde33885a9db1
Sim <simshard@gmail.com>
2025-02-28 04:39:07 +0000

    template layout and stylee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="todo-app-container">
-      <div className="todo-app bg-blue-600">
+      <div className="todo-app">
         <h2>Todo App</h2>
         
         <form action="#">
@@ -43,7 +43,7 @@ function App() {
                 {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
               </div>
               <button className="x-button">
-                <svg
+                {<svg
                   className="x-button-icon"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -55,13 +55,13 @@ function App() {
                     strokeWidth={2}
                     d="M6 18L18 6M6 6l12 12"
                   />
-                </svg>
+                </svg> /* */}
               </button>
             </li>
           ))}
         </ul>
 
-        <div className="check-all-container">
+        <div className="check-all-container mt-6 p-4">
           <div>
             <div className="button">Check All</div>
           </div>
@@ -69,12 +69,12 @@ function App() {
           <span>3 items remaining</span>
         </div>
 
-        <div className="other-buttons-container">
+        <div className="other-buttons-container bg-gray-400 mt-8 p-4">
           <div>
             <button className="button filter-button filter-button-active">
               All
             </button>
-            <button className="button filter-button">Active</button>
+            <button className="button filter-button text-xs font-light">Active</button>
             <button className="button filter-button">Completed</button>
           </div>
           <div>
